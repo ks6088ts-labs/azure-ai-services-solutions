39f56aac7ca4fb92e35901646a6240363b950466
ks6088ts <ks6088ts@gmail.com>
2024-05-03 12:46:26 +0900

    support azure functions using FastAPI

--- function_app.py
+++ function_app.py
@@ -1,3 +1,8 @@
 import azure.functions as func
 
-app = func.FunctionApp()
+from backend.fastapi import app as fastapi_app
+
+app = func.AsgiFunctionApp(
+    app=fastapi_app,
+    http_auth_level=func.AuthLevel.ANONYMOUS,
+)
