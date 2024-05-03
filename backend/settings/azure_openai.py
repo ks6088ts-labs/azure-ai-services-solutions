5d7617446206a2f4da4d9cb6718e1e4fdf55501a
ks6088ts <ks6088ts@gmail.com>
2024-05-03 12:47:04 +0900

    rename environment variables to avoid conflicts

--- backend/settings/azure_openai.py
+++ backend/settings/azure_openai.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    endpoint: str = "https://<aoai-name>.openai.azure.com/"
-    api_key: str = "<aoai-api-key>"
-    api_version: str = "2024-02-01"
-    embedding_model: str = "text-embedding-ada-002"
-    gpt_model: str = "gpt-35-turbo"
+    azure_openai_endpoint: str = "https://<aoai-name>.openai.azure.com/"
+    azure_openai_api_key: str = "<aoai-api-key>"
+    azure_openai_api_version: str = "2024-02-01"
+    azure_openai_embedding_model: str = "text-embedding-ada-002"
+    azure_openai_gpt_model: str = "gpt-35-turbo"
 
     model_config = SettingsConfigDict(
         env_file="azure_openai.env",
